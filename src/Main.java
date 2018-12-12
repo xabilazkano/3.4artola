90d65c23d732380c29f21a6ac09ec26ab7f1df59
Xabi <xlazcanoit18dw@ikzubirimanteo.com>
2018-12-12 08:35:37 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 import com.zubiri.agenda.Contact;
 import com.zubiri.agenda.Person;
 import com.zubiri.agenda.Agenda;
@@ -10,8 +8,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Person pertsona = new Person("");
-		Contact kontaktua = new Contact(pertsona,"","");
-		Agenda agend=new Agenda();
+		Contact kontaktua = new Contact(pertsona, "", "");
+		Agenda agend = new Agenda();
 		Scanner sc = new Scanner(System.in);
 		boolean flag = true;
 		while (flag) {
@@ -40,14 +38,14 @@ public class Main {
 						System.out.println("Weight:");
 						int weight = sc.nextInt();
 						System.out.println("Height:");
-						int height =sc.nextInt();
+						int height = sc.nextInt();
 						System.out.println("DNI:");
 						String dni = sc.next();
 						System.out.println("Telephone number");
 						String phone = sc.next();
 						System.out.println("Address");
 						String address = sc.next();
-						Person create = new Person(name, age, weight,height, dni);
+						Person create = new Person(name, age, weight, height, dni);
 						Contact sortu = new Contact(create, phone, address);
 						agend.addContact(sortu);
 						System.out.println("Succesfuly added.");
@@ -57,13 +55,11 @@ public class Main {
 					case 2:
 						System.out.println("Enter the name of the contact:");
 						name = sc.next();
-						if (agend.findContact(name)==-1) {
+						if (agend.findContact(name) == -1) {
 							System.out.println("Contact not found");
-						}
-						else {
+						} else {
 							System.out.println(agend.getContact(agend.findContact(name)).contactToString());
 						}
-						
 
 						break;
 
@@ -75,26 +71,58 @@ public class Main {
 						break;
 
 					case 4:
+						
 						System.out.println("Enter the name of the contact");
 						name = sc.next();
-						System.out.println("New name:");
-						String name2 = sc.next();
-						System.out.println("New age:");
-						 age = sc.nextInt();
-						System.out.println("New weight:");
-						 weight = sc.nextInt();
-						System.out.println("New height:");
-						 height =sc.nextInt();
-						System.out.println("New DNI:");
-						 dni = sc.next();
-						System.out.println("New telephone number");
-						 phone = sc.next();
-						System.out.println("New address");
-						 address = sc.next();
-						Person person = new Person(name2, age, weight,height, dni);
-						Contact contact = new Contact(person, phone, address);
-					
-						agend.modifyContact(contact,agend.findContact(name));
+						Contact cont=agend.getContact(agend.findContact(name));
+						int index = agend.findContact(name);
+		
+						System.out.println("What do you want to change?");
+						String change = sc.next();
+						switch (change) {
+						case "name":
+							System.out.println("New name:");
+							String name2 = sc.next();
+							agend.getContact(agend.findContact(name)).getPerson().setName(name2);
+							break;
+
+						case "age":
+							System.out.println("New age:");
+							age = sc.nextInt();
+							agend.getContact(agend.findContact(name)).getPerson().setAge(age);
+							break;
+
+						case "weight":
+							System.out.println("New weight:");
+							weight = sc.nextInt();
+							agend.getContact(agend.findContact(name)).getPerson().setWeight(weight);
+							break;
+
+						case "height":
+							System.out.println("New height:");
+							height = sc.nextInt();
+							agend.getContact(agend.findContact(name)).getPerson().setHeight(height);
+							break;
+
+						case "dni":
+							System.out.println("New DNI:");
+							dni = sc.next();
+							agend.getContact(agend.findContact(name)).getPerson().setDni(dni);
+							break;
+						case "telephone":
+							System.out.println("New telephone number");
+							phone = sc.next();
+							agend.getContact(agend.findContact(name)).setNumber(phone);
+							break;
+						case "address":
+							System.out.println("New address");
+							address = sc.next();
+							agend.getContact(agend.findContact(name)).setAddress(address);
+							break;
+
+						}
+
+						agend.modifyContact(cont, index);
 						break;
 
 					}
